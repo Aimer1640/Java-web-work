0181567bf730cde559bab9a075f431230aa4f739
Aimer <2806987816@qq.com>
2023-06-03 21:25:06 +0800

    3号终版

--- lab_25/src/main/java/com/example/WorkApplication.java
+++ lab_25/src/main/java/com/example/WorkApplication.java
@@ -1,17 +1,15 @@
 package com.example;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan("com.example.mapper")
 @SpringBootApplication
-public class Lab241Application {
+public class WorkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Lab241Application.class, args);
+        SpringApplication.run(WorkApplication.class, args);
     }
 
 }
